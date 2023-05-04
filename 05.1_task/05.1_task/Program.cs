febc963a816a36a9db2b03314983b8b2e1f315a2
alinll <alineedemyanchuk@gmail.com>
2023-05-04 12:35:02 +0300

    5.1 task

---
+++ 05.1_task/05.1_task/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace _05._1_task
+{
+
+    interface IFlyable
+    {
+        void Fly()
+        {
+            Console.WriteLine("Object can fly");
+        }
+    }
+
+    class Bird : IFlyable
+    {
+        public string Name { get; set; }
+        public bool canFly { get; set; }
+        public void Fly()
+        {
+            if (canFly)
+            {
+                Console.WriteLine($"{Name} can fly");
+            }
+            else if(!canFly)
+            {
+                Console.WriteLine($"{Name} can't fly");
+            }
+        }
+    }
+
+    class Plane : IFlyable
+    {
+        public string Mark { get; set; }
+        public float HighFly { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine($"{Mark} plane can fly up to {HighFly}m");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird bird1 = new()
+            {
+                Name = "Peregrine falcon",
+                canFly = true
+            };
+
+            Bird bird2 = new()
+            {
+                Name = "Penguin",
+                canFly = false
+            };
+
+            Plane plane = new()
+            {
+                Mark = "AN-225 Mriya",
+                HighFly = 11000
+            };
+
+            IFlyable[] flyingObject = new IFlyable[]
+            {
+                bird1,
+                bird2,
+                new Bird() {Name = "Chicken", canFly = true},
+                plane,
+                new Plane() {Mark = "AN-124 Ruslan", HighFly = 9000}
+            };
+
+            foreach(IFlyable flyingObj in flyingObject)
+            {
+                flyingObj.Fly();
+            }
+        }
+    }
+}
